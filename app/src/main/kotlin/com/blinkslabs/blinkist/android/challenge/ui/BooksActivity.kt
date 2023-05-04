83b461be72777c5046fa842e36b5c2bd09b5c34b
junior-od <opeyemi.oduberu@gmail.com>
2023-05-04 12:45:58 +0100

    added compose to the project

--- app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/ui/BooksActivity.kt
+++ app/src/main/kotlin/com/blinkslabs/blinkist/android/challenge/ui/BooksActivity.kt
@@ -3,49 +3,71 @@ package com.blinkslabs.blinkist.android.challenge.ui
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.ui.platform.ViewCompositionStrategy
 import androidx.lifecycle.Observer
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
-import com.blinkslabs.blinkist.android.challenge.R
 import com.blinkslabs.blinkist.android.challenge.data.model.Book
+import com.blinkslabs.blinkist.android.challenge.databinding.ActivityBooksBinding
 import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class BooksActivity : AppCompatActivity() {
 
-    //todo comment this
+    // todo comment this
     @Inject lateinit var booksViewModelFactory: BooksViewModelFactory
 
-    //todo let BooksViewModel use @Hiltviewmodel
+    // todo let BooksViewModel use @Hiltviewmodel
     private val viewModel by viewModels<BooksViewModel> { booksViewModelFactory }
 
     private lateinit var recyclerAdapter: BookListRecyclerAdapter
 
-    private lateinit var recyclerView : RecyclerView
-    private lateinit var swipeRefreshLayout : SwipeRefreshLayout
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var swipeRefreshLayout: SwipeRefreshLayout
+
+    private lateinit var binding: ActivityBooksBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_books)
-        recyclerView = findViewById(R.id.recyclerView)
-        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout)
+        supportActionBar?.hide()
+        binding = ActivityBooksBinding.inflate(layoutInflater)
+        setContentView(binding.root).apply {
+            binding.composeView.apply {
+                setViewCompositionStrategy(ViewCompositionStrategy.DisposeOnViewTreeLifecycleDestroyed)
+
+                setContent {
+                    MaterialTheme {
+                        Text("ddd")
+                    }
+                }
+            }
+        }
+//        recyclerView = findViewById(R.id.recyclerView)
+//        swipeRefreshLayout = findViewById(R.id.swipeRefreshLayout)
 
 //        (application as BlinkistChallengeApplication).component.inject(this)
 
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerAdapter = BookListRecyclerAdapter()
-        recyclerView.adapter = recyclerAdapter
+//        recyclerView.layoutManager = LinearLayoutManager(this)
+//        recyclerAdapter = BookListRecyclerAdapter()
+//        recyclerView.adapter = recyclerAdapter
+//
+//        swipeRefreshLayout.setOnRefreshListener { viewModel.fetchBooks() }
+//
+        viewModel.books().observe(
+            this,
+            Observer { books ->
+                Timber.tag("yeah").d(books?.get(0)?.author.toString())
 
-        swipeRefreshLayout.setOnRefreshListener { viewModel.fetchBooks() }
-
-        viewModel.books().observe(this, Observer { books ->
-            showBooks(books)
-            hideLoading()
-        })
-
-        showLoading()
+//            showBooks(books)
+//            hideLoading()
+            }
+        )
+//
+//        showLoading()
         viewModel.fetchBooks()
     }
 
